fd6316831c068da93868940d636dea579d51c56f
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-02-01 19:52:49 -0600

    Mucho mejor :v

---
+++ VHDL/Example.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity PinNumbers is 
+	port (
+			a, b, c, d, e: in std_logic;
+			x1, x2, x3, x4, x5, x6: out std_logic
+		);
+end PinNumbers;
+
+architecture Gate of PinNumbers is
+begin
+	x1 <= a and b and c and d and e;
+	x6 <= not c;
+end Gate;
