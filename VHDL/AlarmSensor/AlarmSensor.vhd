d332e13807a2428e51668f090ffef796521af598
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-02-08 20:12:48 -0600

    Añadimos puertos

--- VHDL/AlarmSensor/AlarmSensor.vhd
+++ VHDL/AlarmSensor/AlarmSensor.vhd
@@ -1,18 +1,40 @@
-LIBRARY ieee;   
-USE ieee.STD_LOGIC_1164.all;    
+-- ==================================================== --
+-- ==============    ALARM SENSOR   =================== --
+-- ==================================================== --
+LIBRARY IEEE;   
+USE IEEE.STD_LOGIC_1164.ALL;    
     
 ENTITY AlarmSensor IS     
+    
     PORT (
         A, B, Reference: in STD_LOGIC_VECTOR(2 DOWNTO 0);
         Selector: in STD_LOGIC;    
         Display: out STD_LOGIC_VECTOR(6 DOWNTO 0)
     );
+
+    ATTRIBUTE PIN_NUMBERS OF AlarmSensor : ENTITY IS
+        "Display(0): 15 "   &   
+        "Display(1): 16 "   &   
+        "Display(2): 17 "   &   
+        "Display(3): 18 "   &   
+        "Display(4): 19 "   &   
+        "Display(5): 20 "   &   
+        "Display(6): 21 "   & 
+        "A(0): 2 "          &   
+        "A(1): 3 "          &   
+        "B(6): 4 "          &
+        "B(4): 5 "          &   
+        "Reference(0): 6 "  &   
+        "Reference(1): 7 "  &
+        "Selector: 8 ";    
+
 END AlarmSensor;  
 
 
 ARCHITECTURE Behave OF AlarmSensor IS 
     
     SIGNAL SelectorOut, ComparatorOut: STD_LOGIC_VECTOR(2 DOWNTO 0);
+    
     CONSTANT GreaterSymbol : STD_LOGIC_VECTOR(6 DOWNTO 0) := "1111000";
     CONSTANT LessSymbol    : STD_LOGIC_VECTOR(6 DOWNTO 0) := "1001110";
     CONSTANT EqualSymbol   : STD_LOGIC_VECTOR(6 DOWNTO 0) := "1001000";
@@ -27,7 +49,7 @@ ARCHITECTURE Behave OF AlarmSensor IS
         -- =============  COMPARATOR  ============== --   
         Comparator: PROCESS(SelectorOut, Reference)
         BEGIN
-            IF (SelectorOut > Reference) THEN
+            IF (SelectorOut > Reference)    THEN
                 ComparatorOut <= "100";
             ELSIF (SelectorOut = Reference) THEN
                 ComparatorOut <= "010";
