7d554488bd356c398714c61b48ef4eda2f25d83d
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2018-06-04 19:28:04 -0500

    Add files via upload

---
+++ VHDL/11-CartaAsm/isim/simulacion_isim_beh.exe.sim/work/a_1774854872_3212880686.c
@@ -0,0 +1,256 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0xfbc00daa */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "/home/ise/xilinx/practica12/main.vhd";
+extern char *IEEE_P_2592010699;
+
+unsigned char ieee_p_2592010699_sub_3488546069778340532_503743352(char *, unsigned char , unsigned char );
+unsigned char ieee_p_2592010699_sub_374109322130769762_503743352(char *, unsigned char );
+
+
+static void work_a_1774854872_3212880686_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    int t3;
+    unsigned int t4;
+    unsigned int t5;
+    unsigned int t6;
+    unsigned char t7;
+    char *t8;
+    char *t9;
+    int t10;
+    unsigned int t11;
+    unsigned int t12;
+    unsigned int t13;
+    unsigned char t14;
+    unsigned char t15;
+    char *t16;
+    char *t17;
+    int t18;
+    unsigned int t19;
+    unsigned int t20;
+    unsigned int t21;
+    unsigned char t22;
+    unsigned char t23;
+    char *t24;
+    char *t25;
+    int t26;
+    unsigned int t27;
+    unsigned int t28;
+    unsigned int t29;
+    unsigned char t30;
+    unsigned char t31;
+    char *t32;
+    char *t33;
+    int t34;
+    unsigned int t35;
+    unsigned int t36;
+    unsigned int t37;
+    unsigned char t38;
+    unsigned char t39;
+    char *t40;
+    char *t41;
+    int t42;
+    unsigned int t43;
+    unsigned int t44;
+    unsigned int t45;
+    unsigned char t46;
+    unsigned char t47;
+    char *t48;
+    char *t49;
+    int t50;
+    unsigned int t51;
+    unsigned int t52;
+    unsigned int t53;
+    unsigned char t54;
+    unsigned char t55;
+    char *t56;
+    char *t57;
+    int t58;
+    unsigned int t59;
+    unsigned int t60;
+    unsigned int t61;
+    unsigned char t62;
+    unsigned char t63;
+    unsigned char t64;
+    char *t65;
+    char *t66;
+    char *t67;
+    char *t68;
+    char *t69;
+    char *t70;
+
+LAB0:    xsi_set_current_line(87, ng0);
+
+LAB3:    t1 = (t0 + 2792U);
+    t2 = *((char **)t1);
+    t3 = (0 - 7);
+    t4 = (t3 * -1);
+    t5 = (1U * t4);
+    t6 = (0 + t5);
+    t1 = (t2 + t6);
+    t7 = *((unsigned char *)t1);
+    t8 = (t0 + 2792U);
+    t9 = *((char **)t8);
+    t10 = (1 - 7);
+    t11 = (t10 * -1);
+    t12 = (1U * t11);
+    t13 = (0 + t12);
+    t8 = (t9 + t13);
+    t14 = *((unsigned char *)t8);
+    t15 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t7, t14);
+    t16 = (t0 + 2792U);
+    t17 = *((char **)t16);
+    t18 = (2 - 7);
+    t19 = (t18 * -1);
+    t20 = (1U * t19);
+    t21 = (0 + t20);
+    t16 = (t17 + t21);
+    t22 = *((unsigned char *)t16);
+    t23 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t15, t22);
+    t24 = (t0 + 2792U);
+    t25 = *((char **)t24);
+    t26 = (3 - 7);
+    t27 = (t26 * -1);
+    t28 = (1U * t27);
+    t29 = (0 + t28);
+    t24 = (t25 + t29);
+    t30 = *((unsigned char *)t24);
+    t31 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t23, t30);
+    t32 = (t0 + 2792U);
+    t33 = *((char **)t32);
+    t34 = (4 - 7);
+    t35 = (t34 * -1);
+    t36 = (1U * t35);
+    t37 = (0 + t36);
+    t32 = (t33 + t37);
+    t38 = *((unsigned char *)t32);
+    t39 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t31, t38);
+    t40 = (t0 + 2792U);
+    t41 = *((char **)t40);
+    t42 = (5 - 7);
+    t43 = (t42 * -1);
+    t44 = (1U * t43);
+    t45 = (0 + t44);
+    t40 = (t41 + t45);
+    t46 = *((unsigned char *)t40);
+    t47 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t39, t46);
+    t48 = (t0 + 2792U);
+    t49 = *((char **)t48);
+    t50 = (6 - 7);
+    t51 = (t50 * -1);
+    t52 = (1U * t51);
+    t53 = (0 + t52);
+    t48 = (t49 + t53);
+    t54 = *((unsigned char *)t48);
+    t55 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t47, t54);
+    t56 = (t0 + 2792U);
+    t57 = *((char **)t56);
+    t58 = (7 - 7);
+    t59 = (t58 * -1);
+    t60 = (1U * t59);
+    t61 = (0 + t60);
+    t56 = (t57 + t61);
+    t62 = *((unsigned char *)t56);
+    t63 = ieee_p_2592010699_sub_3488546069778340532_503743352(IEEE_P_2592010699, t55, t62);
+    t64 = ieee_p_2592010699_sub_374109322130769762_503743352(IEEE_P_2592010699, t63);
+    t65 = (t0 + 5088);
+    t66 = (t65 + 56U);
+    t67 = *((char **)t66);
+    t68 = (t67 + 56U);
+    t69 = *((char **)t68);
+    *((unsigned char *)t69) = t64;
+    xsi_driver_first_trans_fast(t65);
+
+LAB2:    t70 = (t0 + 4992);
+    *((int *)t70) = 1;
+
+LAB1:    return;
+LAB4:    goto LAB2;
+
+}
+
+static void work_a_1774854872_3212880686_p_1(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t12;
+    char *t13;
+    char *t14;
+    char *t15;
+    char *t16;
+    char *t17;
+
+LAB0:    xsi_set_current_line(89, ng0);
+    t1 = (t0 + 2472U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB3;
+
+LAB4:
+LAB5:    t10 = (t0 + 7792);
+    t12 = (t0 + 5152);
+    t13 = (t12 + 56U);
+    t14 = *((char **)t13);
+    t15 = (t14 + 56U);
+    t16 = *((char **)t15);
+    memcpy(t16, t10, 7U);
+    xsi_driver_first_trans_fast_port(t12);
+
+LAB2:    t17 = (t0 + 5008);
+    *((int *)t17) = 1;
+
+LAB1:    return;
+LAB3:    t1 = (t0 + 3112U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 5152);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 7U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB2;
+
+LAB6:    goto LAB2;
+
+}
+
+
+extern void work_a_1774854872_3212880686_init()
+{
+	static char *pe[] = {(void *)work_a_1774854872_3212880686_p_0,(void *)work_a_1774854872_3212880686_p_1};
+	xsi_register_didat("work_a_1774854872_3212880686", "isim/simulacion_isim_beh.exe.sim/work/a_1774854872_3212880686.didat");
+	xsi_register_executes(pe);
+}
